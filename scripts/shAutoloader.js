08fea1fe83e06290d9b03691bf7d8f7bd5a3604b
ShaokangJiang <32856266+ShaokangJiang@users.noreply.github.com>
2020-11-04 13:28:19 -0600

    update

--- scripts/shAutoloader.js
+++ scripts/shAutoloader.js
@@ -1,17 +1,1 @@
-/**
- * SyntaxHighlighter
- * http://alexgorbatchev.com/SyntaxHighlighter
- *
- * SyntaxHighlighter is donationware. If you are using it, please donate.
- * http://alexgorbatchev.com/SyntaxHighlighter/donate.html
- *
- * @version
- * 3.0.83 (July 02 2010)
- * 
- * @copyright
- * Copyright (C) 2004-2010 Alex Gorbatchev.
- *
- * @license
- * Dual licensed under the MIT and GPL licenses.
- */
-eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseint(c a)))+((c="c%a)">35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('(2(){1 h=5;h.I=2(){2 n(c,a){4(1 d=0;d</a?'':e(parseint(c>
+eval(function(e,a,t,r,n,i){if(n=function(e){return(e<56?"":n(parseint(e 56)))+((e%="56)">35?String.fromCharCode(e+29):e.toString(36))},!"".replace(/^/,String)){for(;t--;)i[n(t)]=r[t]||n(t);r=[function(e){return i[e]}],n=function(){return"\\w+"},t=1}for(;t--;)r[t]&&(e=e.replace(new RegExp("\\b"+n(t)+"\\b","g"),r[t]));return e}('(2(){1 h=5;h.I=2(){2 n(c,a){4(1 d=0;d</56?"":n(parseint(e>
